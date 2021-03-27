86cfe15ba72cbac68d88e22b265f40e867eb18ad
petr-klepetko <72223801+petr-klepetko@users.noreply.github.com>
2021-03-27 20:04:56 +0100

    update

--- Led/Includes/head.php
+++ Led/Includes/head.php
@@ -1,5 +1,5 @@
 <?php
-$currentColor = json_decode(file_get_contents("color/currentColor.json"), true);
+$currentColor = json_decode(file_get_contents("Color/currentColor.json"), true);
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +11,7 @@ $currentColor = json_decode(file_get_contents("color/currentColor.json"), true);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="/led/ledControl.js"></script>
+    <script src="/Led/ledControl.js"></script>
     <link rel="stylesheet" href="/led/styles.css">
 </head>
 
--- Led/Includes/topMenu.php
+++ Led/Includes/topMenu.php
@@ -1,7 +1,7 @@
 <header>
     <div class="topMenu">
-        <a href="/led/manualColor/"><div class="topMenuItem"><p>Barva</p></div></a>
-        <a href="/led/"><div class="topMenuItem"><p>Intenzita</p></div></a>
-        <a href="/led/settings/"><div class="topMenuItem"><p>Nastavení</p></div></a>
+        <a href="/Led/ManualColor/"><div class="topMenuItem"><p>Barva</p></div></a>
+        <a href="/Led/"><div class="topMenuItem"><p>Intenzita</p></div></a>
+        <a href="/Led/Settings/"><div class="topMenuItem"><p>Nastavení</p></div></a>
     </div>
 </header>
